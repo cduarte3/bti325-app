b284198a523fd5209a46e9a8d97f6086346dd565
Christian Duarte <cduarte3@myseneca.ca>
2022-10-03 16:17:14 -0400

    final commit before cyclic

--- data-service.js
+++ data-service.js
@@ -1,16 +1,65 @@
-let employees = [];
-let departments = [];
+var employees = [];
+var departments = [];
+var managers = [];
 var fs = require('fs');
+const { resolve } = require('path');
 
 module.exports. initialize  = function (){
-    fs.readFile('./data/employees.json',(err,data)=>{
-        if (err) reject("Failure to read file employees.json!");
-        employees = JSON.parse(data);
-        if(employees != null){
-            fs.readFile('./data/departments.json',(err,data)=>{
-                if (err) reject("Failure to read file departments.json!");
-                departments = JSON.parse(data)
+        return new Promise(function(resolve, reject)
+        {
+            fs.readFile('./data/employees.json',(err,data)=>
+            {
+                if (err) reject("Failure to read file employees.json!");
+                employees = JSON.parse(data);
+                if(employees != null)
+                {
+                    fs.readFile('./data/departments.json',(err,data)=>
+                    {
+                        if (err) reject("Failure to read file departments.json!");
+                        departments = JSON.parse(data)
+                    })
+                }
+                if(!err){
+                    resolve("Both files were read successfully");
+                }
             })
+        });
+}
+
+module.exports. getAllEmployees = function (){
+    return new Promise(function(resolve, reject){
+        if(employees.length > 0){
+            resolve(employees);
+        }
+        else{
+            reject("Employee array was empty");
+        }
+    })
+}
+
+module.exports. getManagers = function (){
+    return new Promise(function(resolve, reject){
+        for(i = 0; i < employees.length; ++i){
+            if(employees[i].isManager == true){
+                managers.push(employees[i]);
+            }
+        }
+        if(managers.length > 0){
+            resolve(managers);
+        }
+        else{
+            reject("Manager array was empty");
+        }
+    })
+}
+
+module.exports. getDepartments = function (){
+    return new Promise(function(resolve, reject){
+        if(departments.length > 0){
+            resolve(departments);
+        }
+        else{
+            reject("Department array was empty");
         }
     })
 }
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-var D = require("./data-service.js");
+/*************************************************************************
+* BTI325– Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic 
+Policy. No part * of this assignment has been copied manually or electronically from any 
+other source 
+* (including 3rd party web sites) or distributed to other students.
+* 
+* Name: Christian Duarte Student ID: 158217208 Date: October 3rd 2022
+*
+* Your app’s URL (from Cyclic) : ______________________________________________
+*
+*************************************************************************/
 var express = require("express"); // Include express.js module
 var service = require("../bti325-app/data-service.js"); // include data - service module
 var app = express();
 var HTTP_PORT = process.env.PORT || 8080;
+var Obj = {"message": null};
 
 var path = require("path"); // include moduel path to use __dirname, and function path.join()
 
@@ -15,27 +27,24 @@ function onHttpStart(){
 
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+    res.sendFile(path.join(__dirname, 'views/', 'home.html'));
 });
 
  // setup another route to listen on /about
  app.get("/about", function (req, res){
-    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+    res.sendFile(path.join(__dirname, 'views/', 'about.html'));
 });
 
-//setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
-
 app.get("/employees", function (req, res){
-    res.send("TODO: Return all employees");
+    service.getAllEmployees() .then(emp => res.json(emp)) .catch((err) => {res.json(Obj)});
 });
 
 app.get("/managers", function (req, res){
-    res.send("TODO: Return all employees who isManager = true");
+    service.getManagers() .then(man=> res.json(man)) .catch((err) => {res.json(Obj)});
 });
 
 app.get("/departments", function (req, res){
-    res.send("TODO: Return all the departments listed");
+    service.getDepartments() .then (dep => res.json(dep)) .catch((err) => {res.json(Obj)});
 });
 
 app.get("*", function (req, res){
@@ -43,4 +52,6 @@ app.get("*", function (req, res){
     res.statusCode(404);
 });
 
-D.initialize();
+service.initialize()
+.then(() => { app.listen(HTTP_PORT, onHttpStart) }) //setup http server to listen on HTTP_PORT
+.catch((error)=>{ console.log(error)});
