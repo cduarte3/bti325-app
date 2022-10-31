47dc3f9ba15858dea620b526673148da24778df9
Christian Duarte <cduarte3@myseneca.ca>
2022-10-31 15:23:50 -0400

    before heroku commit

--- data-service.js
+++ data-service.js
@@ -3,6 +3,7 @@ var departments = [];
 var managers = [];
 var fs = require('fs');
 const { resolve } = require('path');
+const { isNullOrUndefined } = require('util');
 
 module.exports. initialize  = function (){
         return new Promise(function(resolve, reject)
@@ -60,6 +61,86 @@ module.exports. getDepartments = function (){
         }
         else{
             reject("Department array was empty");
+        }
+    })
+}
+
+module.exports. addEmployee = function(employeeData){
+    return new Promise(function(resolve, reject){
+        if (employeeData.isManager == undefined){
+            employeeData.isManager = false;
+        }
+        else{
+            employeeData.isManager = true;
+        }
+        employeeData.employeeNum = employees.length + 1;
+        employees.push(employeeData);
+        resolve();
+    })
+}
+
+module.exports. getEmployeesByStatus = function(status){
+    return new Promise(function(resolve, reject){
+        if(employees.length > 0){
+            var E2 = [];
+            for(var i = 0; i < employees.length; i++){
+                if(employees[i].status == status){
+                    E2.push(employees[i]);
+                }
+            }
+            resolve(E2);
+        }
+        else{
+            reject("Employees did not have the specified status");
+        }
+    })
+}
+
+module.exports. getEmployeesByDepartment = function(department){
+    return new Promise(function(resolve, reject){
+        if(employees.length > 0){
+            var E3 = [];
+            for(var i = 0; i < employees.length; i++){
+                if(employees[i].department == department){
+                    E3.push(employees[i]);
+                }
+            }
+            resolve(E3);
+        }
+        else{
+            reject("Employees did not have the specified Department");
+        }
+    })
+}
+
+module.exports. getEmployeesByManager = function(manager){
+    return new Promise(function(resolve, reject){
+        if(employees.length > 0){
+            var E4 = [];
+            for(var i = 0; i < employees.length; i++){
+                if(employees[i].employeeManagerNum == manager){
+                    E4.push(employees[i]);
+                }
+            }
+            resolve(E4);
+        }
+        else{
+            reject("Employees did not have the specified Manager");
+        }
+    })
+}
+
+module.exports. getEmployeeByNum = function(num){
+    return new Promise(function(resolve, reject){
+        if(employees.length > 0){
+            for(var i = 0; i < employees.length; i++){
+                if(employees[i].employeeNum == parseInt(num)){
+                    resolve (employees[i]);
+                }
+            }
+        }
+        else{
+            reject("Employee did not have the specified Employee Num");
         }
     })
 }
--- server.js
+++ server.js
@@ -7,16 +7,31 @@ other source
 * 
 * Name: Christian Duarte Student ID: 158217208 Date: October 3rd 2022
 *
-* Your app’s URL (from Cyclic) : ______________________________________________
+* Your app’s URL (from Cyclic) : https://fine-ruby-battledress.cyclic.app/
 *
 *************************************************************************/
 var express = require("express"); // Include express.js module
 var service = require("./data-service.js"); // include data - service module
+var multer = require("multer"); // Include multer.js module
 var app = express();
+var fs = require('fs');
 var HTTP_PORT = process.env.PORT || 8080;
 var Obj = {"message": null};
+const storage = multer.diskStorage({
+    destination: "./public/images/uploaded",
+    filename: function (req, file, cb){
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer ({storage:storage});
 
 var path = require("path"); // include moduel path to use __dirname, and function path.join()
+const { fstat } = require("fs");
+
+// form without file upload
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 // call this function after the http server starts listening for requests
@@ -30,12 +45,37 @@ app.get("/", function(req, res){
 });
 
  // setup another route to listen on /about
- app.get("/about", function (req, res){
+app.get("/about", function (req, res){
     res.sendFile(path.join(__dirname, 'views/', 'about.html'));
 });
 
+ // setup another route to listen on /employees/add
+app.get("/employees/add", function (req, res){
+    res.sendFile(path.join(__dirname, 'views/', 'addEmployee.html'));
+});
+
+ // setup another route to listen on /images/add
+app.get("/images/add", function (req, res){
+    res.sendFile(path.join(__dirname, 'views/', 'addImage.html'));
+});
+
 app.get("/employees", function (req, res){
-    service.getAllEmployees() .then(emp => res.json(emp)) .catch((err) => {res.json(Obj)});
+    if(req.query.status){
+        service.getEmployeesByStatus(req.query.status) .then(emp => res.json(emp)) .catch((err) => {res.json(Obj)});
+    }
+    else if(req.query.department){
+        service.getEmployeesByDepartment(req.query.department) .then(emp => res.json(emp)) .catch((err) => {res.json(Obj)});
+    }
+    else if(req.query.manager){
+        service.getEmployeesByManager(req.query.manager) .then(emp => res.json(emp)) .catch((err) => {res.json(Obj)});
+    }
+    else{
+        service.getAllEmployees() .then(emp => res.json(emp)) .catch((err) => {res.json(Obj)});
+    }
+});
+
+app.get("/employee/:value", function (req, res){
+    service.getEmployeeByNum(req.params.value) .then(man=> res.json(man)) .catch((err) => {res.json(Obj)});
 });
 
 app.get("/managers", function (req, res){
@@ -46,11 +86,26 @@ app.get("/departments", function (req, res){
     service.getDepartments() .then (dep => res.json(dep)) .catch((err) => {res.json(Obj)});
 });
 
-app.get("*", function (req, res){
-    res.send("Page Not Found");
-    res.statusCode(404);
+// adding images
+app.get("/images", (req, res)=>{
+    fs.readdir(__dirname + "/public/images/uploaded", (err, items)=>{
+        res.json({images : items});
+    })
+});
+app.post("/images/add", upload.single("imageFile"),(req,res)=>{
+    res.redirect("/images");
+});
+
+//adding employees
+app.post("/employees/add", (req,res)=>{
+    service.addEmployee(req.body) .then (res.redirect("/employees"));
 });
 
 service.initialize()
 .then(() => { app.listen(HTTP_PORT, onHttpStart) }) //setup http server to listen on HTTP_PORT
 .catch((error)=>{ console.log(error)});
+
+app.use((req, res)=>{
+    res.send("Page Not Found");
+    res.statusCode(404);
+});
